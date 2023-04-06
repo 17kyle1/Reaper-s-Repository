e29fee4def45b10cbd5fb9ec325fb97e488861a1
Aaron Turner <Turneraaron97@gmail.com>
2023-04-06 19:44:48 -0400

    Created schema tables

--- reaper's repository/Develop/db/schema.sql
+++ reaper's repository/Develop/db/schema.sql
@@ -5,4 +5,23 @@ USE reapers_db;
 
 CREATE TABLE battles (
     id INT PRIMARY KEY
+    weapons TEXT NOT NULL
+    trench_warfare TEXT NOT NULL
+    chemical_weapons TEXT NOT NULL
+    deaths INT NOT NULL
+
+)
+CREATE TABLE disease (
+    id INT PRIMARY KEY
+    spanish_flu TEXT NOT NULL
+    trench_foot TEXT NOT NULL
+    Typhoid TEXT NOT NULL
+    deaths INT NOT NULL
+)
+CREATE TABLE lasting_effects (
+    id INT PRIMARY KEY
+    bolshevik_revolution TEXT NOT NULL
+    world_war_two TEXT NOT NULL
+    league_of_nations TEXT NOT NULL
+    deaths INT NOT NULL
 )
---
+++ reaper's repository/Develop/db/seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO battles (weapons, trench_warfare, chemical_weapons, deaths)
+VALUES (
+
+)
+INSERT INTO disease (spanish_flu, trench_foot, Typhoid, deaths)
+VALUES (
+
+)
+
+INSERT INTO lasting_effects (bolsheviks, world_war_two, league_of_nations, deaths)
+VALUES (
+
+)
