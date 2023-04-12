8c7ab831f63925780f838644f299808d686e846a
Aaron Turner <Turneraaron97@gmail.com>
2023-04-12 17:10:39 -0400

    Added details to Seeds

--- Reaper-Repository/Develop/seeds/Disease_seeds.js
+++ Reaper-Repository/Develop/seeds/Disease_seeds.js
@@ -18,11 +18,9 @@ const diseaseData = [
     {
         id: 3,
         name: 'Influenza',
-        deaths: 0,
-        description: '',
-        relevency: '',
-    },
-    {
+        deaths: 50000000,
+        description: 'Often referred to as the "Spanish Flu". Influenza cost the lives of millions of people, military and civilian throughout the Great War',
+        relevency: 'The Corona Virus is often compared to the Spanish Flu, as both were global pandemics',
         id: 4,
         name: 'Malaria',
         deaths: 0,
